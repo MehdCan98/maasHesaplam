1791c16320260d2cc9c329d27330ec943417508a
Mehdi ÇELİK <105076539+MehdCan98@users.noreply.github.com>
2024-03-03 23:32:49 +0300

    Initial commit

---
+++ src/Maas.java
@@ -0,0 +1,86 @@
+class Employee {
+
+    String name; //Çalışanın adı ve soyadı
+    int salary;//Çalışanın maaşı
+    int workHours; //: Haftalık çalışma saati
+    int hireYear; //: İşe başlangıç yılı
+
+    //double vergiOrani =0.03;
+    double vergiTutari = 0.0;
+    int bonusUcret = 0;
+    double maasHesapArtisi = 0.0;
+
+
+    public Employee(String name, int salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+        tax();
+        bonus();
+        raiseSalary();
+        //print();
+        //toString();
+    }
+
+    public void tax() {
+        /*Çalışanın maaşı 1000 TL'den az ise vergi uygulanmayacaktır.
+        Çalışanın maaşı 1000 TL'den fazla ise maaşının %3'ü kadar vergi uygulanacaktır.*/
+
+        if (this.salary < 1000) {
+
+            this.vergiTutari = 0;
+        }
+        if (this.salary > 1000) {
+            this.vergiTutari = (this.salary * 0.03);
+        }
+
+    }
+
+    public void bonus() {
+        if (this.workHours > 40) {
+            this.bonusUcret = (this.workHours - 40) * 30;
+        }
+        if (this.workHours < 40) {
+            this.bonusUcret = 0;
+        }
+        /*Eğer çalışan haftada 40 saatten fazla çalışmış ise
+        fazladan çalıştığı her saat başına 30 TL olacak şekilde
+        bonus ücretleri hesaplayacaktır.
+         */
+
+    }
+
+    public void raiseSalary() {
+        int hesapYil;
+        hesapYil = 2021 - this.hireYear;
+
+        if (hesapYil < 10) {
+            this.maasHesapArtisi = this.salary * 0.05;
+        }
+        if (hesapYil > 9 && hesapYil < 20) {
+            this.maasHesapArtisi = this.salary * 0.10;
+        }
+        if (hesapYil > 19) {
+            this.maasHesapArtisi = this.salary * 0.15;
+        }
+
+       /* Çalışanın işe başlangıç yılına göre maaş artışını hesaplayacaktır. Şuan ki yılı 2021 olarak alın.
+        Eğer çalışan 10 yıldan az bir süredir çalışıyorsa maaşına %5 zam yapılacaktır.
+        Eğer çalışan 9 yıldan fazla ve 20 yıldan az çalışıyorsa maaşına %10 zam yapılacaktır.
+        Eğer çalışan 19 yıldan fazla çalışıyorsa %15 zam yapılacaktır.*/
+    }
+
+
+    public String toString() {
+        return ("Çalışanın adı ve soyadı : " + this.name
+                + "\nÇalışanın maaşı : " + this.salary
+                + "\nHaftalık çalışma saati: " + this.workHours
+                + "\nİşe başlangıç yılı: " + this.hireYear
+                + "\nVergi Tutarı: " + this.vergiTutari
+                + "\nBonus Ücreti: " + this.bonusUcret
+                + "\nMaaş Artışı: " + this.maasHesapArtisi + "\nVergi ve Bonuslar ile birlikte maaş : " + (this.salary + this.bonusUcret - this.vergiTutari)
+                + "\nToplam Maaş  : " + (this.salary + this.bonusUcret + this.maasHesapArtisi - this.vergiTutari));
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee calisan1 = new Employee("Mehdi Çelik", 2000, 45,1985);//7
+        Employee calisan2 = new Employee("Selim şişman", 3500, 20,2010);//10
+        Employee calisan3 = new Employee("Salim uzun", 3000, 30,2001);//20
+
+        System.out.println(calisan1);
+
+        System.out.println(calisan2);
+
+        System.out.println(calisan3);
+
+
+    }
+
+
+}
